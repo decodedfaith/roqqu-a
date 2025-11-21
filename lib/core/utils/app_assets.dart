75fab60afdf0e027b79e83a677cee5de4f7b255e
decodedfaith <komolafefaith@gmail.com>
2025-11-22 00:30:06 +0100

    Refactor Assets Centralize all asset strings into the new AppAssets class.

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,21 @@
+class AppAssets {
+  // Icons
+  static const String bitcoinIcon = 'assets/icons/bitcoinIcon.svg';
+  static const String ethereumIcon = 'assets/icons/ethereumIcon.svg';
+  static const String bnbIcon = 'assets/icons/bnbIcon.svg';
+  static const String solIcon = 'assets/icons/solIcon.svg';
+  static const String xrpIcon = 'assets/icons/xrp-icon.svg';
+  static const String dogeIcon = 'assets/icons/doge-icon.svg';
+  static const String orbIce = 'assets/icons/orbIce.svg';
+  static const String deposit = 'assets/icons/deposit.svg';
+  static const String buy = 'assets/icons/buy.svg';
+  static const String withdraw = 'assets/icons/withdraw.svg';
+  static const String send = 'assets/icons/send.svg';
+  static const String crown = 'assets/icons/crown.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String headphones = 'assets/icons/headphones.svg';
+  static const String notification = 'assets/icons/bNotification.svg';
+  
+  // Videos/GIFs
+  static const String demoVideo = 'assets/video/demo2.gif';
+}
